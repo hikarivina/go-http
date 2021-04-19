de36dc305feefbc28dd30d7c59df55ad939d026a
Hikarivina <dangnguyenhuu88@gmail.com>
2021-04-20 00:55:53 +0900

    add request header

--- gohttp/client_core.go
+++ gohttp/client_core.go
@@ -15,6 +15,28 @@ func (c *httpClient) do(method string, url string, headers http.Header, body int
 		return nil, errors.New("unable to create a new request")
 	}
 
+	fullHeader := c.getRequestHeaders(headers)
+	request.Header = fullHeader
+
 	return client.Do(request)
 
+}
+
+func (c *httpClient) getRequestHeaders(requestHeaders http.Header) http.Header {
+	result := make(http.Header)
+
+	// Add common headers to the reuqest:
+	for header, value := range c.Headers {
+		if len(value) > 0 {
+			result.Set(header, value[0])
+		}
+	}
+	// Add custom headers to the reuqest:
+	for header, value := range requestHeaders {
+		if len(value) > 0 {
+			result.Set(header, value[0])
+		}
+	}
+
+	return result
 }
--- sample.go
+++ sample.go
@@ -3,14 +3,27 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 
 	"github.com/hikarivina/go-http.git/gohttp"
 )
 
-func main() {
+var (
+	githubHttpClient = getGithubClient()
+)
+
+func getGithubClient() gohttp.HttpClient {
 	client := gohttp.New()
 
-	response, err := client.Get("https://api.github.com", nil)
+	commonHeader := make(http.Header)
+	commonHeader.Set("Authorization", "Bearer ABC-123")
+	client.SetHeaders(commonHeader)
+
+	return client
+}
+
+func main() {
+	response, err := githubHttpClient.Get("https://api.github.com", nil)
 	if err != nil {
 		panic(err)
 	}
